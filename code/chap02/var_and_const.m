86932c1d1bba432ab08c4f26c18d01af78d65cc3
xkzhai-ubuntuAIO <xkzhai@ubuntuAIO.com>
2022-11-05 10:51:13 +0800

    add var_const

---
+++ code/chap02/var_and_const.m
@@ -0,0 +1,19 @@
+clear;
+clc
+
+%% rules of varNames
+% 1) Upper and Lower are different
+
+a = 1
+A = 2
+
+school = 'usts'
+School = 'usts2'
+
+whos
+
+% 2) Consists of a-zA-Z, number, underline
+clear;
+% 1a = 1
+a1 = 1
+a_1 = 2
